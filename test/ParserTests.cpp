59932a1131d437a9b200a75905ca784e7a1ea3a4
radx64 <radekteam@gmail.com>
2019-03-13 21:55:19 +0100

    Multiply PT tests

--- test/ParserTests.cpp
+++ test/ParserTests.cpp
@@ -67,6 +67,45 @@ TEST(ParserShould, createCorrectTreeForPlusOperatorWithMoreElements)
     EXPECT_EQ(left_branch->left_child->parent->value, Token(Type::PLUS)); 
 
     EXPECT_EQ(left_branch->right_child->value, Token(Type::NUMBER, "2"));
-    EXPECT_EQ(left_branch->left_child->parent->value, Token(Type::PLUS)); 
+    EXPECT_EQ(left_branch->right_child->parent->value, Token(Type::PLUS)); 
+    Parser::destroy(expression);
+}
+
+TEST(ParserShould, createCorrectTreeForPlusAndMultiplyOperators)
+{
+    std::list<Token> input = 
+    {
+        Token{Type::NUMBER, "1"},
+        Token{Type::PLUS},
+        Token{Type::NUMBER, "2"},
+        Token{Type::MULTIPLY},
+        Token{Type::NUMBER, "3"}
+    };
+    Expression* expression = Parser::build(input);
+
+    /* Expected parse tree 
+         +          
+        1 *
+         2 3
+    */
+
+    EXPECT_EQ(expression->value, Token(Type::PLUS));
+    EXPECT_EQ(expression->parent, nullptr);
+
+    EXPECT_EQ(expression->left_child->value, Token(Type::NUMBER, "1"));
+    EXPECT_EQ(expression->left_child->parent->value, Token(Type::PLUS)); 
+    EXPECT_EQ(expression->right_child->value, Token(Type::MULTIPLY));    
+    EXPECT_EQ(expression->right_child->parent->value, Token(Type::PLUS)); 
+
+     Expression* right_branch = expression->right_child;
+
+    EXPECT_EQ(right_branch->value, Token(Type::MULTIPLY));
+    EXPECT_EQ(right_branch->parent->value, Token(Type::PLUS));
+
+    EXPECT_EQ(right_branch->left_child->value, Token(Type::NUMBER, "2"));
+    EXPECT_EQ(right_branch->left_child->parent->value, Token(Type::MULTIPLY)); 
+
+    EXPECT_EQ(right_branch->right_child->value, Token(Type::NUMBER, "3"));
+    EXPECT_EQ(right_branch->right_child->parent->value, Token(Type::MULTIPLY)); 
     Parser::destroy(expression);
 }
